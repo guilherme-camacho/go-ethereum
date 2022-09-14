a6502a341c256cae5f1debbaec558904991cf6e1
Guilherme camacho <guilherme.camacho@financialhub.dev>
2022-09-14 19:32:33 -0300

    Update module_node.go

--- cmd/puppeth/module_node.go
+++ cmd/puppeth/module_node.go
@@ -50,12 +50,11 @@ RUN echo 'geth init /genesis.json' > geth.sh
 RUN echo 'mkdir -p /root/.ethereum/keystore/ && cp /signer.json /root/.ethereum/keystore/' >> geth.sh
 {{end}}
 
-RUN	echo 'exec geth --syncmode 'full' --networkid {{.NetworkID}} --port {{.Port}} --nat extip:{{.IP}} \
-	--ethstats {{.Ethstats}} \
+RUN	echo 'exec geth --syncmode 'full' --networkid {{.NetworkID}} --port {{.Port}} --nat extip:{{.IP}} --ethstats {{.Ethstats}} \
 	{{if .Bootnodes}}--bootnodes {{.Bootnodes}}{{end}} \
 	--http --http.addr 0.0.0.0 --http.port {{.WebPort}} --http.api admin,eth,miner,net,txpool,personal,web3 \
 	--ws --ws.port {{.WebSocketPort}} --ws.addr 0.0.0.0 --ws.api web3,eth \
-	{{if .Unlock}}--unlock 0 --mine --password /signer.pass --allow-insecure-unlock --miner.gastarget {{.GasTarget}} --miner.gasprice {{.GasPrice}}' >> geth.sh
+	{{if .Unlock}}--unlock 0 --mine --password /signer.pass --allow-insecure-unlock --miner.gastarget {{.GasTarget}} --miner.gasprice {{.GasPrice}}{{end}}' >> geth.sh
 
 ENTRYPOINT ["/bin/sh", "geth.sh"]
 `
